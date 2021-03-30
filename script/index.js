f184a900e6bfffa145b03a46e830fc371fb31697
Anshul <anshulsharma9317@gmail.com>
2021-03-30 21:13:45 +0530

    final commit

---
+++ script/index.js
@@ -0,0 +1,32 @@
+function showSignIn() {
+    var modal = document.getElementById("signin-modal");
+    modal.style.display = "flex";
+}
+function hideSignIn() {
+    var modal = document.getElementById("signin-modal");
+    modal.style.display = "none";
+}
+function showSignUp() {
+    var modal = document.getElementById("signup-modal");
+    modal.style.display = "flex";
+}
+function hideSignUp() {
+    var modal = document.getElementById("signup-modal");
+    modal.style.display = "none";
+}
+
+function showCreatePost() {
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "flex";
+}
+    
+  
+  function hideCreatePost(){
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "none";
+  }
+  
+  function BlogLists(){
+    location.href = "html/bloglist.html"
+  }
+  
